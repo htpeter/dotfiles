b8ef679dc4a33c8aa9fff1420203a0001f64d824
peter <kouvaris.peter@gmail.com>
2018-10-30 21:22:20 -0400

    folder org system

---
+++ langage_setup/installGo1.11.sh
@@ -0,0 +1,15 @@
+curl -O https://dl.google.com/go/go1.11.linux-amd64.tar.gz
+sha256sum go1.11.linux-amd64.tar.gz
+tar -xvf go1.11.linux-amd64.tar.gz
+
+apt-get -y install git
+
+chown -R root:root ./go
+mv go /usr/local
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.bashrc
+
+mkdir ~/go && cd ~/go
+echo 'src pkg bin' | xargs mkdir
+
+echo "Run source ~/.bashrc"
